3b4e7c80f44f536f2e4aaeafc3397a9a2f72f998
Ulisespl <91744936+Ulisespl@users.noreply.github.com>
2025-09-01 10:12:29 -0600

    2.5. Detección de colisiones
    Reproduce el siguiente vídeo tutorial Interactive Book 08 Collision detection y utiliza la información allí expuesta para agregar la detección de colisiones para la aplicación interactiva en desarrollo.

--- Ulises/Assets/Scripts/SegundoScript.cs
+++ Ulises/Assets/Scripts/SegundoScript.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
-public class PrimerScript : MonoBehaviour
+public class SegundoScript : MonoBehaviour
 {
+
+    Rigidbody2D rb2d;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
